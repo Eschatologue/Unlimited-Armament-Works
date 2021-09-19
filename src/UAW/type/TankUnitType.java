93592e69c69eb26ed0314ab7620ff9241fc50c4c
ItzAlen <alen2001valentino@gmail.com>
2021-09-19 10:57:50 +0700

    Tank unit polishing 6

--- src/UAW/type/TankUnitType.java
+++ src/UAW/type/TankUnitType.java
@@ -2,27 +2,29 @@
 package UAW.type;
 
 import UAW.type.weapon.TurretedWeapon;
+import arc.graphics.Color;
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.TextureRegion;
 import arc.math.Angles;
+import arc.math.Mathf;
 import arc.struct.Seq;
+import arc.util.Tmp;
+import mindustry.content.Blocks;
 import mindustry.entities.units.WeaponMount;
 import mindustry.gen.MechUnit;
+import mindustry.gen.Mechc;
 import mindustry.gen.Unit;
 import mindustry.gen.UnitEntity;
 import mindustry.type.UnitType;
+import mindustry.world.blocks.environment.Floor;
 
 public class TankUnitType extends UnitType {
-    public final Seq<TurretedWeapon> TurrettedWeapon = new Seq<>();
     public TankUnitType(String name) {
         super(name);
+        flying = false;
         constructor = MechUnit::create;
-        mechSideSway = 0f;
-        mechFrontSway = 0f;
-        mechStepParticles = true;
+        mechFrontSway = mechSideSway = 0f;
         mechStride = 0.5f;
-        trailX = 5.5f;
-        trailY = -4f;
     }
 }
 
