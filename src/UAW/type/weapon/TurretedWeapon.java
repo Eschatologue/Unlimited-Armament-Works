7744271314f93457ffaf3d4cfe5c411a8afb326c
ItzAlen <alen2001valentino@gmail.com>
2021-09-19 09:28:54 +0700

    Tank unit polishing 2

--- src/UAW/type/weapon/TurretedWeapon.java
+++ src/UAW/type/weapon/TurretedWeapon.java
@@ -2,11 +2,14 @@
 package UAW.type.weapon;
 
 import arc.Core;
+import arc.graphics.Blending;
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.TextureRegion;
 import arc.math.Angles;
+import arc.math.Mathf;
 import mindustry.entities.units.WeaponMount;
 import mindustry.gen.Unit;
+import mindustry.graphics.Drawf;
 import mindustry.type.Weapon;
 
 public class TurretedWeapon extends Weapon {
@@ -22,14 +25,45 @@ public class TurretedWeapon extends Weapon {
         super.load();
         turret = Core.atlas.find(name + "-turret");
     }
-    public void draw(Unit unit, WeaponMount tWeapon){
-        super.draw(unit, tWeapon);
-        float rotation = unit.rotation - 90,
-            weaponRotation = rotation + (rotate ? tWeapon.rotation : 0),
-            wx = unit.x + Angles.trnsx(rotation, x, y) + Angles.trnsx(weaponRotation, 0, 0),
-            wy = unit.y + Angles.trnsy(rotation, x, y) + Angles.trnsy(weaponRotation, 0, 0);
-            Draw.rect(turret,
-                    wx, wy, turret.width * Draw.scl, region.height * Draw.scl, weaponRotation);
+
+    public void draw(Unit unit, WeaponMount mount){
+        float
+                rotation = unit.rotation - 90,
+                weaponRotation  = rotation + (rotate ? mount.rotation : 0),
+                recoil = -((mount.reload) / reload * this.recoil),
+                wx = unit.x + Angles.trnsx(rotation, x, y) + Angles.trnsx(weaponRotation, 0, recoil),
+                wy = unit.y + Angles.trnsy(rotation, x, y) + Angles.trnsy(weaponRotation, 0, recoil);
+
+        if(shadow > 0){
+            Drawf.shadow(wx, wy, shadow);
+        }
+
+        if(outlineRegion.found() && top){
+            Draw.rect(outlineRegion,
+                    wx, wy,
+                    outlineRegion.width * Draw.scl * -Mathf.sign(flipSprite),
+                    region.height * Draw.scl,
+                    weaponRotation);
+        }
+
+        Draw.rect(region,
+                wx, wy,
+                region.width * Draw.scl * -Mathf.sign(flipSprite),
+                region.height * Draw.scl,
+                weaponRotation);
+
+        if(heatRegion.found() && mount.heat > 0){
+            Draw.color(heatColor, mount.heat);
+            Draw.blend(Blending.additive);
+            Draw.rect(heatRegion,
+                    wx, wy,
+                    heatRegion.width * Draw.scl * -Mathf.sign(flipSprite),
+                    heatRegion.height * Draw.scl,
+                    weaponRotation);
+            Draw.blend();
+            Draw.color();
+        }
+        Draw.rect(turret, wx, wy, weaponRotation);
     }
 }
 
