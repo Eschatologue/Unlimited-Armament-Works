cf8ab5534f8263f877e4e85c482f50baecf14cbb
ItzAlen <alen2001valentino@gmail.com>
2021-10-19 22:46:52 +0700

    Effects Polishing 6 - copter rotor resprite

--- src/UAW/type/Rotor.java
+++ src/UAW/type/Rotor.java
@@ -1,6 +1,7 @@
 package UAW.type;
 
 import arc.Core;
+import arc.graphics.Color;
 import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.util.Time;
@@ -8,7 +9,7 @@ import mindustry.gen.Unit;
 
 public class Rotor {
     public final String name;
-    public TextureRegion bladeRegion, topRegion;
+    public TextureRegion bladeRegion, bladeOutlineRegion, topRegion, topRegionOutline;
 
     public float x = 0f;
     public float y = 0f;
@@ -22,21 +23,27 @@ public class Rotor {
 
     public void load() {
         bladeRegion = Core.atlas.find(name);
+        bladeOutlineRegion = Core.atlas.find(name + "-outline");
         topRegion = Core.atlas.find(name + "-top");
+        topRegionOutline = Core.atlas.find(name + "-top-outline");
     }
 
     public void draw(Unit unit) {
-        float RotorSpeedScl = rotationSpeed;
+        float rotorSpeedScl = rotationSpeed;
         float rx = unit.x + Angles.trnsx(unit.rotation - 90, x, y);
         float ry = unit.y + Angles.trnsy(unit.rotation - 90, x, y);
 
         if (unit.health() < 0 || unit.dead) {
-            RotorSpeedScl = Mathf.lerpDelta(rotationSpeed, 0f, 0.5f);
+            rotorSpeedScl = Mathf.lerpDelta(rotationSpeed, 1f, 0.05f);
         }
         for (int i = 0; i < bladeCount; i++) {
-            float angle = ((i * 360f / bladeCount + (((Time.time * RotorSpeedScl))) % 360));
+            float angle = ((i * 360f / bladeCount + (((Time.time * rotorSpeedScl))) % 360));
+            Draw.rect(bladeOutlineRegion, rx, ry, bladeOutlineRegion.width * Draw.scl, bladeOutlineRegion.height * Draw.scl, angle);
+            Draw.mixcol(Color.white, unit.hitTime);
             Draw.rect(bladeRegion, rx, ry, bladeRegion.width * Draw.scl, bladeRegion.height * Draw.scl, angle);
         }
+        Draw.rect(topRegionOutline, rx, ry, topRegionOutline.width * Draw.scl, topRegionOutline.height * Draw.scl, unit.rotation - 90);
+        Draw.mixcol(Color.white, unit.hitTime);
         Draw.rect(topRegion, rx, ry, topRegion.width * Draw.scl, topRegion.height * Draw.scl, unit.rotation - 90);
     }
 }
