684c8f8c1fc6d4acd55535e8c2f4d4ec7c2d7193
ItzAlen <alen2001valentino@gmail.com>
2021-11-03 19:49:19 +0700

    Calogrenant implentation 20

--- src/UAW/type/Rotor.java
+++ src/UAW/type/Rotor.java
@@ -36,7 +36,7 @@ public class Rotor {
         float ry = unit.y + Angles.trnsy(unit.rotation - 90, x, y);
 
         if (unit.health() < 0 || unit.dead) {
-            rotorSpeedScl = Mathf.lerpDelta(rotationSpeed, (rotationSpeed / 4), unit.type.fallSpeed);
+            rotorSpeedScl = Mathf.lerpDelta(rotationSpeed, (rotationSpeed / 4),(unit.type.fallSpeed * 100));
         }
         for (int i = 0; i < bladeCount; i++) {
             float angle = ((i * 360f / bladeCount + (((Time.time * rotorSpeedScl))) % 360));
