673294b469c4ca8b687212b7ebeca471db872a37
ItzAlen <alen2001valentino@gmail.com>
2021-11-04 21:35:49 +0700

    Calogrenant implentation 32

--- src/UAW/ai/types/SeekAI.java
+++ src/UAW/ai/types/SeekAI.java
@@ -35,7 +35,7 @@ public class SeekAI extends AIController {
             if (state.rules.waves && unit.team == state.rules.defaultTeam) {
                 Tile spawner = getClosestSpawner();
                 if (spawner != null && unit.within(spawner, state.rules.dropZoneRadius + 120f)) move = false;
-                if (core != null && unit.within(core, state.rules.dropZoneRadius + 120f)) move = false;
+                if (core != null && unit.within(core, unit.range() / 3)) move = false;
             }
             if (target != null && unit.within(target, unit.type.range) && !Vars.world.raycast(unit.tileX(), unit.tileY(), target.tileX(), target.tileY(), (x, y) -> {
                 for (Point2 p : Geometry.d4c) {
