43065ff3e825e141189966a3c06226e3fdfa6df2
ItzAlen <alen2001valentino@gmail.com>
2021-11-05 23:59:34 +0700

    Calogrenant implentation 35

--- src/UAW/type/CopterUnitType.java
+++ src/UAW/type/CopterUnitType.java
@@ -2,15 +2,17 @@ package UAW.type;
 
 import UAW.ai.types.CopterAI;
 import arc.graphics.g2d.Draw;
-import arc.math.Mathf;
-import arc.struct.*;
+import arc.math.*;
+import arc.struct.Seq;
 import arc.util.Time;
+import mindustry.content.Fx;
 import mindustry.gen.*;
-import mindustry.type.*;
+import mindustry.type.UnitType;
 
 public class CopterUnitType extends UnitType {
     public final Seq<Rotor> rotors = new Seq<>();
     public float spinningFallSpeed = 0;
+    public float fallSmokeX = 0f, fallSmokeY = -5f, fallSmokeChance = 0.5f;
 
     public CopterUnitType(String name) {
         super(name);
@@ -25,16 +27,16 @@ public class CopterUnitType extends UnitType {
     @Override
     public void update(Unit unit) {
         float unitFallSpin;
+        float rx = unit.x + Angles.trnsx(unit.rotation - 90, fallSmokeX, fallSmokeY);
+        float ry = unit.y + Angles.trnsy(unit.rotation - 90, fallSmokeX, fallSmokeY);
         super.update(unit);
-        if (unit.isFlying() && spinningFallSpeed > 0) {
+        if (unit.isFlying() && spinningFallSpeed > 0 || unit.dead() || unit.health < 0) {
             if (unit.dead() || unit.health < 0) {
                 unitFallSpin = Time.delta * spinningFallSpeed;
                 unit.rotation += unitFallSpin;
-            }
-        }
-        if (unit.dead() || unit.health < 0) {
-            for(Rotor rotor : rotors){
-                rotor.rotorSpeed = Time.time * Mathf.lerpDelta(rotor.rotorSpeed, 0, spinningFallSpeed);
+                if (Mathf.chanceDelta(fallSmokeChance)) {
+                    Fx.fallSmoke.at(rx, ry);
+                }
             }
         }
     }
