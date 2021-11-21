703c21e93d3216b082438ca17c746e9a722d0007
ItzAlen <alen2001valentino@gmail.com>
2021-11-22 06:30:42 +0700

    Jet Bomber Implementation 42

--- src/UAW/type/units/JetUnitType.java
+++ src/UAW/type/units/JetUnitType.java
@@ -47,7 +47,7 @@ public class JetUnitType extends UnitType {
 				UAWFxDynamic.jetTrail(trailLength).at(cx2, cy2, trailWidth, trailColor);
 			}
 		}
-		omniMovement = !unit.isPlayer();
+		omniMovement = !unit.isPlayer() || unit.isShooting;
 		/*
 		trailLeft.update(cx, cy);
 		trailRight.update(cx2, cy2);*/
