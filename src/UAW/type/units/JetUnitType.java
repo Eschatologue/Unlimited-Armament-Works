369d73bec395c6c14f995db588c79222a01ef218
ItzAlen <alen2001valentino@gmail.com>
2021-11-21 13:02:59 +0700

    Jet Bomber Implementation 13

--- src/UAW/type/units/JetUnitType.java
+++ src/UAW/type/units/JetUnitType.java
@@ -1,20 +1,22 @@
 package UAW.type.units;
 
 import arc.graphics.Color;
-import arc.graphics.g2d.Draw;
-import arc.math.Angles;
+import arc.math.*;
+import mindustry.content.Fx;
 import mindustry.gen.*;
-import mindustry.graphics.*;
+import mindustry.graphics.Pal;
 import mindustry.type.UnitType;
 
 public class JetUnitType extends UnitType {
 	public float trailX = 5f;
-	public float trailY = 0f;
+	public float trailY = -1f;
 	public int trailLength = 6;
 	public float trailWidth = 4f;
 	public Color trailColor = Pal.bulletYellowBack;
+	/*
 	public Trail trailLeft = new Trail(trailLength);
 	public Trail trailRight = new Trail(trailLength);
+	*/
 
 	public JetUnitType(String name) {
 		super(name);
@@ -28,17 +30,26 @@ public class JetUnitType extends UnitType {
 	@Override
 	public void update(Unit unit) {
 		super.update(unit);
-		float cx = unit.x + Angles.trnsx(unit.rotation - 90, trailX, -trailY);
+		float cx = unit.x + Angles.trnsx(unit.rotation - 90, -trailX, -trailY);
 		float cy = unit.y + Angles.trnsx(unit.rotation - 90, -trailX, -trailY);
+		float cx2 = unit.x + Angles.trnsx(unit.rotation - 90, trailX, -trailY);
+		float cy2 = unit.y + Angles.trnsx(unit.rotation - 90, trailX, -trailY);
+		if (unit.moving()) {
+			if (Mathf.chanceDelta(1)) {
+				Fx.artilleryTrail.at(cx, cy, trailWidth, trailColor);
+				Fx.artilleryTrail.at(cx2, cy2, trailWidth, trailColor);
+			}
+		}
+		/*
 		trailLeft.update(cx, cy);
-		trailRight.update(cx, cy);
+		trailRight.update(cx2, cy2);*/
 	}
-
+/*
 	@Override
 	public void draw(Unit unit) {
 		super.draw(unit);
 		Draw.z(Layer.effect);
 		trailLeft.draw(trailColor, trailWidth);
 		trailRight.draw(trailColor, trailWidth);
-	}
+	}*/
 }
