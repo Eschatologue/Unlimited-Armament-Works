60b962ace6af548d1ca2940af37681fe1b58efbf
ItzAlen <alen2001valentino@gmail.com>
2021-12-18 17:39:40 +0700

    Petroleum System Implementation 31

--- src/UAW/world/blocks/liquid/PressurizedConduit.java
+++ src/UAW/world/blocks/liquid/PressurizedConduit.java
@@ -1,17 +1,11 @@
 package UAW.world.blocks.liquid;
 
 import UAW.content.UAWBlock;
-import arc.func.Boolf;
-import arc.math.Mathf;
-import arc.math.geom.*;
-import arc.struct.Seq;
-import mindustry.content.Blocks;
-import mindustry.entities.units.BuildPlan;
 import mindustry.gen.Building;
 import mindustry.type.Liquid;
 import mindustry.world.*;
-import mindustry.world.blocks.distribution.ItemBridge;
-import mindustry.world.blocks.liquid.*;
+import mindustry.world.blocks.liquid.Conduit;
+import mindustry.world.blocks.production.Pump;
 
 public class PressurizedConduit extends Conduit {
 
@@ -23,30 +17,17 @@ public class PressurizedConduit extends Conduit {
 		liquidPressure = 1.5f;
 		leaks = false;
 		placeableLiquid = true;
-	}
-
-	@Override
-	public void init() {
-		super.init();
-		if (junctionReplacement == null) junctionReplacement = UAWBlock.pressurizedLiquidJunction;
-		if (bridgeReplacement == null || !(bridgeReplacement instanceof ItemBridge)) bridgeReplacement = UAWBlock.pressurizedBridgeConduit;
-	}
-
-	@Override
-	public Block getReplacement(BuildPlan req, Seq<BuildPlan> requests){
-		if(junctionReplacement == null) return this;
-
-		Boolf<Point2> cont = p -> requests.contains(o -> o.x == req.x + p.x && o.y == req.y + p.y && o.rotation == req.rotation && (req.block instanceof PressurizedConduit || req.block instanceof LiquidJunction));
-		return cont.get(Geometry.d4(req.rotation)) &&
-			cont.get(Geometry.d4(req.rotation - 2)) &&
-			req.tile() != null &&
-			req.tile().block() instanceof Conduit &&
-			Mathf.mod(req.build().rotation - req.rotation, 2) == 1 ? junctionReplacement : this;
+		junctionReplacement = UAWBlock.pressurizedLiquidJunction;
+		bridgeReplacement = UAWBlock.pressurizedBridgeConduit;
 	}
 
 	@Override
 	public boolean blends(Tile tile, int rotation, int otherX, int otherY, int otherRot, Block otherBlock) {
-		return otherBlock.outputsLiquid && blendsArmored(tile, rotation, otherX, otherY, otherRot, otherBlock) || lookingAt(tile, rotation, otherX, otherY, otherBlock) && otherBlock.hasLiquids && otherBlock instanceof PressurizedConduit;
+		return
+			otherBlock.outputsLiquid
+				&& blendsArmored(tile, rotation, otherX, otherY, otherRot, otherBlock)
+				|| lookingAt(tile, rotation, otherX, otherY, otherBlock)
+				&& otherBlock.hasLiquids;
 	}
 
 	public class PressurizedConduitBuild extends ConduitBuild {
