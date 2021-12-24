bb6a92db65f8a87d21a49f40adf4fe0b4d1d983a
ItzAlen <alen2001valentino@gmail.com>
2021-12-24 19:30:03 +0700

    Petroleum System Implementation 55

--- src/UAW/world/blocks/liquid/PressurizedConduit.java
+++ src/UAW/world/blocks/liquid/PressurizedConduit.java
@@ -28,7 +28,7 @@ public class PressurizedConduit extends ArmoredConduit {
 	@Override
 	public boolean blends(Tile tile, int rotation, int otherX, int otherY, int otherRot, Block otherBlock){
 		return (otherBlock.outputsLiquid
-			&& !(otherBlock instanceof Conduit)
+			|| !(otherBlock instanceof Conduit)
 			|| otherBlock instanceof PressurizedConduit
 			&& blendsArmored(tile, rotation, otherX, otherY, otherRot, otherBlock))
 			|| (lookingAt(tile, rotation, otherX, otherY, otherBlock) && otherBlock.hasLiquids);
