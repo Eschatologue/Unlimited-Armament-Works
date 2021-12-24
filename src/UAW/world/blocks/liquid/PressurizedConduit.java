ad8db10d9ff965ea2498adfd5bdf52ca557e004f
ItzAlen <alen2001valentino@gmail.com>
2021-12-25 00:25:25 +0700

    Petroleum System Implementation 58

--- src/UAW/world/blocks/liquid/PressurizedConduit.java
+++ src/UAW/world/blocks/liquid/PressurizedConduit.java
@@ -1,9 +1,6 @@
 package UAW.world.blocks.liquid;
 
 import UAW.content.UAWBlocks;
-import mindustry.gen.Building;
-import mindustry.type.Liquid;
-import mindustry.world.*;
 import mindustry.world.blocks.liquid.*;
 
 public class PressurizedConduit extends ArmoredConduit {
@@ -23,25 +20,6 @@ public class PressurizedConduit extends ArmoredConduit {
 		super.init();
 		junctionReplacement = UAWBlocks.pressurizedLiquidJunction;
 		bridgeReplacement = UAWBlocks.pressurizedLiquidBridge;
-	}
-
-	@Override
-	public boolean blends(Tile tile, int rotation, int otherX, int otherY, int otherRot, Block otherBlock){
-		return (otherBlock.outputsLiquid
-			|| !(otherBlock instanceof Conduit)
-			|| otherBlock instanceof PressurizedConduit
-			&& blendsArmored(tile, rotation, otherX, otherY, otherRot, otherBlock))
-			|| (lookingAt(tile, rotation, otherX, otherY, otherBlock) && otherBlock.hasLiquids);
-	}
-
-	public class PressurizedConduitBuild extends ArmoredConduitBuild {
-		@Override
-		public boolean acceptLiquid(Building source, Liquid liquid) {
-			return super.acceptLiquid(source, liquid) && (tile == null
-				|| !(source.block instanceof Conduit)
-				|| source.block instanceof PressurizedConduit
-				|| source.tile.absoluteRelativeTo(tile.x, tile.y) == rotation);
-		}
 	}
 }
 
