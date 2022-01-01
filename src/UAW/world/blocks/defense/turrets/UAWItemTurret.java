539868eb36b2eb15fd164ba512c5958399386ebd
ItzAlen <alen2001valentino@gmail.com>
2022-01-01 21:55:31 +0700

    Custom TurretBase 3 - why wont it WORK

--- src/UAW/world/blocks/defense/turrets/UAWItemTurret.java
+++ src/UAW/world/blocks/defense/turrets/UAWItemTurret.java
@@ -40,7 +40,7 @@ public class UAWItemTurret extends ItemTurret {
 	@Override
 	public void load() {
 		super.load();
-		baseRegion = Core.atlas.find(name + "-base", "UAWbase-" + size);
+		baseRegion = Core.atlas.find(name + "-base", "uaw-base-" + size);
 	}
 
 	public class CustomItemTurretBuild extends ItemTurretBuild {
