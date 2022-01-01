79af2d66f43aaa46f98ffaf23cfaa5334bd8d0c1
ItzAlen <alen2001valentino@gmail.com>
2022-01-01 22:09:51 +0700

    Custom TurretBase 6

--- src/UAW/world/blocks/defense/turrets/UAWItemTurret.java
+++ src/UAW/world/blocks/defense/turrets/UAWItemTurret.java
@@ -40,7 +40,7 @@ public class UAWItemTurret extends ItemTurret {
 	@Override
 	public void load() {
 		super.load();
-		baseRegion = Core.atlas.find("turret-base" + size);
+		baseRegion = Core.atlas.find("uaw-turret-base-" + size);
 	}
 
 	public class CustomItemTurretBuild extends ItemTurretBuild {
