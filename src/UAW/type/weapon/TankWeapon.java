e87578311e39bb0036cff5a601cd6e73f7261784
ItzAlen <alen2001valentino@gmail.com>
2022-01-08 21:06:47 +0700

    Tank Unit Rework 16.19

--- src/UAW/type/weapon/TankWeapon.java
+++ src/UAW/type/weapon/TankWeapon.java
@@ -4,7 +4,7 @@ import arc.graphics.Blending;
 import arc.graphics.g2d.Draw;
 import arc.math.*;
 import mindustry.entities.units.WeaponMount;
-import mindustry.gen.Unit;
+import mindustry.gen.*;
 import mindustry.graphics.*;
 
 public class TankWeapon extends UAWWeapon {
@@ -17,36 +17,38 @@ public class TankWeapon extends UAWWeapon {
 
 	@Override
 	public void draw(Unit unit, WeaponMount mount) {
-		float rotation = unit.rotation - 90;
-		float weaponRotation = rotation + (rotate ? mount.rotation : 0);
-		float wx = unit.x + Angles.trnsx(weaponRotation, x + spriteOffsetX, y + spriteOffsetY) + Angles.trnsx(rotation, x, y) + Angles.trnsx(weaponRotation, 0, -mount.recoil);
-		float wy = unit.y + Angles.trnsy(weaponRotation, x + spriteOffsetX, y + spriteOffsetY) + Angles.trnsy(rotation, x, y) + Angles.trnsy(weaponRotation, 0, -mount.recoil);
+		if (unit instanceof Mechc mech) {
+			float rotation = mech.baseRotation() - 90;
+			float weaponRotation = rotation + (rotate ? mount.rotation : 0);
+			float wx = unit.x + Angles.trnsx(weaponRotation, x + spriteOffsetX, y + spriteOffsetY) + Angles.trnsx(rotation, x, y) + Angles.trnsx(weaponRotation, 0, -mount.recoil);
+			float wy = unit.y + Angles.trnsy(weaponRotation, x + spriteOffsetX, y + spriteOffsetY) + Angles.trnsy(rotation, x, y) + Angles.trnsy(weaponRotation, 0, -mount.recoil);
 
-		if (shadow > 0) {
-			Drawf.shadow(wx, wy, shadow);
-		}
+			if (shadow > 0) {
+				Drawf.shadow(wx, wy, shadow);
+			}
 
-		if (top) {
-			drawOutline(unit, mount);
-		}
+			if (top) {
+				drawOutline(unit, mount);
+			}
 
-		Draw.z(weaponLayer);
-		Draw.rect(region,
-			wx, wy,
-			region.width * Draw.scl * -Mathf.sign(flipSprite),
-			region.height * Draw.scl,
-			weaponRotation);
-
-		if (heatRegion.found() && mount.heat > 0) {
-			Draw.color(heatColor, mount.heat);
-			Draw.blend(Blending.additive);
-			Draw.rect(heatRegion,
+			Draw.z(weaponLayer);
+			Draw.rect(region,
 				wx, wy,
-				heatRegion.width * Draw.scl * -Mathf.sign(flipSprite),
-				heatRegion.height * Draw.scl,
+				region.width * Draw.scl * -Mathf.sign(flipSprite),
+				region.height * Draw.scl,
 				weaponRotation);
-			Draw.blend();
-			Draw.color();
+
+			if (heatRegion.found() && mount.heat > 0) {
+				Draw.color(heatColor, mount.heat);
+				Draw.blend(Blending.additive);
+				Draw.rect(heatRegion,
+					wx, wy,
+					heatRegion.width * Draw.scl * -Mathf.sign(flipSprite),
+					heatRegion.height * Draw.scl,
+					weaponRotation);
+				Draw.blend();
+				Draw.color();
+			}
 		}
 	}
 }
