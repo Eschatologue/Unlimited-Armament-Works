9e4dfc34df5326f3fd71f8ac08ac770ccdd208cf
ItzAlen <alen2001valentino@gmail.com>
2022-01-09 08:27:58 +0700

    Tank Unit Rework 16.29

--- src/UAW/type/weapon/TankWeapon.java
+++ src/UAW/type/weapon/TankWeapon.java
@@ -18,10 +18,11 @@ public class TankWeapon extends UAWWeapon {
 	public void drawOutline(Unit unit, WeaponMount mount) {
 		Mechc mech = unit instanceof Mechc ? (Mechc) unit : null;
 		Unit mechUnit = (Unit) mech;
-		float rotation = mech.baseRotation() - 90;
+		float rotation = unit.rotation - 90;
+		float gunRotation = mechUnit.rotation() - 90;
 		float weaponRotation = rotation + (rotate ? mount.rotation : 0);
-		float wx = unit.x + Angles.trnsx(rotation, x, y) + Angles.trnsx(weaponRotation, 0, -mount.recoil);
-		float wy = unit.y + Angles.trnsy(rotation, x, y) + Angles.trnsy(weaponRotation, 0, -mount.recoil);
+		float wx = unit.x + Angles.trnsx(gunRotation, x, y) + Angles.trnsx(weaponRotation, 0, -mount.recoil);
+		float wy = unit.y + Angles.trnsy(gunRotation, x, y) + Angles.trnsy(weaponRotation, 0, -mount.recoil);
 
 		if (outlineRegion.found()) {
 			Draw.rect(outlineRegion,
@@ -36,10 +37,11 @@ public class TankWeapon extends UAWWeapon {
 	public void draw(Unit unit, WeaponMount mount) {
 		Mechc mech = unit instanceof Mechc ? (Mechc) unit : null;
 		Unit mechUnit = (Unit) mech;
-		float rotation = mech.baseRotation() - 90;
+		float rotation = unit.rotation - 90;
+		float gunRotation = mechUnit.rotation() - 90;
 		float weaponRotation = rotation + (rotate ? mount.rotation : 0);
-		float wx = unit.x + Angles.trnsx(rotation, x, y) + Angles.trnsx(weaponRotation, 0, -mount.recoil);
-		float wy = unit.y + Angles.trnsy(rotation, x, y) + Angles.trnsy(weaponRotation, 0, -mount.recoil);
+		float wx = unit.x + Angles.trnsx(gunRotation, x, y) + Angles.trnsx(weaponRotation, 0, -mount.recoil);
+		float wy = unit.y + Angles.trnsy(gunRotation, x, y) + Angles.trnsy(weaponRotation, 0, -mount.recoil);
 
 		if (shadow > 0) {
 			Drawf.shadow(wx, wy, shadow);
