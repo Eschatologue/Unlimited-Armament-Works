bd5966cbc68eb8d099d0f099e962d7d1763d7d44
ItzAlen <alen2001valentino@gmail.com>
2022-01-12 09:08:51 +0700

    Tank Unit Rework 27 - 14 | Tank targeting fixes

--- src/UAW/type/weapon/TankWeapon.java
+++ src/UAW/type/weapon/TankWeapon.java
@@ -106,8 +106,8 @@ public class TankWeapon extends UAWWeapon {
 
 		float
 			weaponRotation = unit.rotation - 90 + (rotate ? mount.rotation : 0),
-			mountX = mechUnit.x + Angles.trnsx(mechRotation - 90, x, y),
-			mountY = mechUnit.y + Angles.trnsy(mechRotation - 90, x, y),
+			mountX = unit.x + Angles.trnsx(mechRotation - 90, x, y),
+			mountY = unit.y + Angles.trnsy(mechRotation - 90, x, y),
 			bulletX = mountX + Angles.trnsx(weaponRotation, this.shootX, this.shootY),
 			bulletY = mountY + Angles.trnsy(weaponRotation, this.shootX, this.shootY),
 			shootAngle = rotate ? weaponRotation + 90 : Angles.angle(bulletX, bulletY, mount.aimX, mount.aimY) + (unit.rotation - unit.angleTo(mount.aimX, mount.aimY));
