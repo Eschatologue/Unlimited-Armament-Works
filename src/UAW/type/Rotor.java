434a5f7063f5ea1651c57dfa8f09e2aadfcde82d
ItzAlen <alen2001valentino@gmail.com>
2022-01-19 20:25:42 +0700

    Copter code polishing 7

--- src/UAW/type/Rotor.java
+++ src/UAW/type/Rotor.java
@@ -42,7 +42,7 @@ public class Rotor {
 
 	public void update(Unit unit) {
 		if (unit.health <= 0 || unit.dead()) {
-			realRotationSpeed = Time.time * (rotorSpeed * unit.elevation);
+			realRotationSpeed = Time.time * (rotorSpeed * (unit.elevation() / 100));
 		} else {
 			realRotationSpeed = Time.time * rotorSpeed;
 		}
