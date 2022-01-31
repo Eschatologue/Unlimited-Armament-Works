57ad8242de8fb83462f0922ae35605b939ab2ec5
ItzAlen <alen2001valentino@gmail.com>
2022-01-31 07:54:10 +0700

    Tank Rework & fix 5.3

--- src/UAW/content/UAWUnitTypes.java
+++ src/UAW/content/UAWUnitTypes.java
@@ -1081,7 +1081,7 @@ public class UAWUnitTypes implements ContentList {
 					}};
 				}},
 				new TankWeapon(name + "-gun") {{
-					layerOffset = -0.01f;
+					layerOffset = -0.1f;
 					targetFlags = new BlockFlag[]{BlockFlag.extinguisher, null};
 					x = 0f;
 					y = 0f;
@@ -1191,7 +1191,7 @@ public class UAWUnitTypes implements ContentList {
 					}};
 				}},
 				new TankWeapon(name + "-gun") {{
-					layerOffset = -0.01f;
+					layerOffset = -0.02f;
 					x = 0f;
 					y = 0f;
 					shootY = 26f;
