3935cfb503bcdff5e32483193284a5904b921aa2
ItzAlen <alen2001valentino@gmail.com>
2022-02-13 07:13:18 +0700

    Blending fix 2

--- src/UAW/world/blocks/liquid/PressurizedConduit.java
+++ src/UAW/world/blocks/liquid/PressurizedConduit.java
@@ -38,7 +38,7 @@ public class PressurizedConduit extends Conduit {
 	@Override
 	public boolean blends(Tile tile, int rotation, int otherX, int otherY, int otherRot, Block otherBlocks) {
 		return otherBlocks.hasLiquids
-			&& (blendsArmored(tile, rotation, otherX, otherY, otherRot, otherBlocks) && !(otherBlocks instanceof Conduit) || otherBlocks instanceof PressurizedConduit)
+			&& (blendsArmored(tile, rotation, otherX, otherY, otherRot, otherBlocks) && !(otherBlocks instanceof Conduit))
 			&& (otherBlocks.outputsLiquid || lookingAt(tile, rotation, otherX, otherY, otherBlocks))
 			&& (lookingAtEither(tile, rotation, otherX, otherY, otherRot, otherBlocks));
 	}
