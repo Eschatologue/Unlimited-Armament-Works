c7d0452c2bd539b6a3b22479308d8177c1cdbf36
ItzAlen <alen2001valentino@gmail.com>
2022-02-20 18:12:18 +0700

    Gas system implementation 7.1

--- src/UAW/content/blocks/UAWBlocksLogistic.java
+++ src/UAW/content/blocks/UAWBlocksLogistic.java
@@ -110,15 +110,14 @@ public class UAWBlocksLogistic implements ContentList {
 			requirements(Category.liquid, BuildVisibility.sandboxOnly, with());
 			alwaysUnlocked = true;
 		}};
-		copperGasBridge = new GasBridge("copper-gas-bridge") {{
-			requirements(Category.liquid, with(
-				Items.metaglass, 8,
-				Items.copper, 8
-			));
-			size = 1;
-			health = 75;
-			range = 6;
-		}};
+//		copperGasBridge = new GasBridge("copper-gas-bridge") {{
+//			requirements(Category.liquid, with(
+//				Items.metaglass, 8,
+//				Items.copper, 8
+//			));
+//			size = 1;
+//			health = 75;
+//		}};
 		copperGasJunction = new GasJunction("copper-gas-junction") {{
 			requirements(Category.liquid, with(
 				Items.metaglass, 2,
