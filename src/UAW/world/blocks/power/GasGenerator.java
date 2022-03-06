4b57f91b22c51dda5868433407d412573a46bd18
ItzAlen <alen2001valentino@gmail.com>
2022-03-06 19:40:44 +0700

    Gas system implementation 17.4 | Steam to Power Implementation

--- src/UAW/world/blocks/power/GasGenerator.java
+++ src/UAW/world/blocks/power/GasGenerator.java
@@ -3,15 +3,13 @@ package UAW.world.blocks.power;
 import arc.Core;
 import arc.graphics.g2d.TextureRegion;
 import arc.math.Mathf;
-import gas.type.Gas;
 import gas.world.blocks.power.GasPowerGenerator;
-import gas.world.consumers.GasConsume;
-import mindustry.world.consumers.ConsumeType;
 
 public class GasGenerator extends GasPowerGenerator {
 	public TextureRegion rotatorRegion1, rotatorRegion2, topRegion;
 
 	public float minimumGas = 0.1f;
+	public float warmupSpeed = 0.01f;
 
 	public boolean drawSteam = false;
 	public int steamParticleCount = 25;
@@ -36,8 +34,13 @@ public class GasGenerator extends GasPowerGenerator {
 		float totalTime;
 
 		@Override
-		public float getPowerProduction(){
-			return powerProduction;
+		public void updateTile() {
+			super.updateTile();
+			if (gasses.currentAmount() > minimumGas) {
+				warmup = Mathf.lerpDelta(warmup, 1f, warmupSpeed * timeScale);
+			} else warmup = Mathf.lerpDelta(warmup, 0f, 0.01f);
+
+			productionEfficiency += warmup * edelta();
 		}
 	}
 }
