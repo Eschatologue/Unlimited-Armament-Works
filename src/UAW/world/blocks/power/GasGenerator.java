99c0f33fcda706f22ab25dd5db3ebf2fdeab23b6
ItzAlen <alen2001valentino@gmail.com>
2022-03-06 21:41:39 +0700

    Gas system implementation 17.8 | Steam to Power Implementation

--- src/UAW/world/blocks/power/GasGenerator.java
+++ src/UAW/world/blocks/power/GasGenerator.java
@@ -2,6 +2,7 @@ package UAW.world.blocks.power;
 
 import UAW.graphics.UAWPal;
 import arc.Core;
+import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.util.Time;
@@ -10,11 +11,13 @@ import mindustry.graphics.Drawf;
 
 public class GasGenerator extends GasPowerGenerator {
 	protected static final Rand rand = new Rand();
-	public TextureRegion rotatorRegion1, rotatorRegion2, topRegion;
+	public TextureRegion heatRegion, rotatorRegion1, rotatorRegion2, topRegion;
+	public Color heatColor = Color.valueOf("ff5512");
 
 	public float warmupSpeed = 0.001f;
 	public float rotatorSpeed = 12f;
 
+	public boolean steamTop = false, steamBottom = true;
 	public int steamParticleCount = 25;
 	public float steamParticleLifetime = 60f;
 	public float steamParticleSpreadRadius = 7f;
@@ -32,6 +35,16 @@ public class GasGenerator extends GasPowerGenerator {
 		topRegion = Core.atlas.find(name + "-top");
 	}
 
+	@Override
+	public TextureRegion[] icons() {
+		return new TextureRegion[]{
+			region,
+			rotatorRegion1.found() ? rotatorRegion1 : Core.atlas.find("clear"),
+			rotatorRegion2.found() ? rotatorRegion2 : Core.atlas.find("clear"),
+			topRegion.found() ? topRegion : Core.atlas.find("clear")
+		};
+	}
+
 	public class GasGeneratorBuild extends GasPowerGenerator.GasGeneratorBuild {
 		float warmup;
 		float totalTime;
@@ -39,10 +52,12 @@ public class GasGenerator extends GasPowerGenerator {
 		@Override
 		public void draw() {
 			super.draw();
-			drawSteam();
+			if (heatRegion.found()) drawHeat();
+			if (steamBottom) drawSteam();
 			if (rotatorRegion1.found()) Drawf.spinSprite(rotatorRegion1, x, y, rotatorSpeed * warmup);
-			if (rotatorRegion2.found()) Drawf.spinSprite(rotatorRegion2, x, y, rotatorSpeed * warmup);
+			if (rotatorRegion2.found()) Drawf.spinSprite(rotatorRegion2, x, y, -rotatorSpeed * warmup);
 			if (topRegion.found()) Draw.rect(topRegion, x, y);
+			if (steamTop) drawSteam();
 		}
 
 		public void drawSteam() {
@@ -57,6 +72,16 @@ public class GasGenerator extends GasPowerGenerator {
 				Fill.circle(x + Angles.trnsx(angle, len), y + Angles.trnsy(angle, len), steamParticleSize * fout * warmup);
 			}
 			Draw.blend();
+			Draw.reset();
+		}
+
+		public void drawHeat() {
+			Draw.color(heatColor);
+			Draw.alpha(warmup * 0.6f * (1f - 0.3f + Mathf.absin(Time.time, 3f, 0.3f)));
+			Draw.blend(Blending.additive);
+			Draw.rect(heatRegion, x, y);
+			Draw.blend();
+			Draw.color();
 			Draw.reset();
 		}
 
